4a40f68890d8eed1cb33404da405fe6d64f3d4a8
ofentse loeto <ohentseloeto610@gmail.com>
2023-11-07 14:48:25 +0000

    Created the auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Implemetation of an Auth class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Authorization method
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        User method that return none
+        """
+        return None
