5641108dfbc9a08d5191185d53ed0ae0562ce600
ofentse loeto <ohentseloeto610@gmail.com>
2023-11-15 13:01:57 +0000

    Create the user object

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,6 +2,8 @@
 """
 Db module
 """
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -14,6 +16,8 @@ class DB:
     """
     The database class
     """
+    Session = sessionmaker()
+
     def __init__(self) -> None:
         """
         Initialize a new db instace
@@ -36,19 +40,44 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """
         Add user to the database
+
+        Args:
+          - email(str): User's email
+          - hashed_password: User's hashed_password
+
+        Return:
+          - Created user object
         """
 
         # Create a new User instance.
-        new_user = User(email=email, hashed_password=hashed_password)
+        user = User(email=email, hashed_password=hashed_password)
 
         # Add a new user to the session.
-        self._session.add(new_user)
+        self._session.add(user)
 
         # Commit changes to the database
         self._session.commit()
 
         # Refresh the user instance to get the updated ID from the database
-        self._session.refresh(new_user)
+        self._session.refresh(user)
 
         # Return the created user object
-        return new_user
+        return user
+
+    def find_user_by(self, **kwargs):
+        """
+        """
+        session = self.Session()
+        try:
+            user = session.query(User).filter_by(**kwargs).first()
+
+            if user is None:
+                raise NoResultFound("No user found")
+
+            return user
+        except InvalidRequestError as e:
+            session.rollback()
+            raise e
+
+        finally:
+            session.close()
