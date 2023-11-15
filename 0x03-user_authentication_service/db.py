c143f2ec79d07b59cc79d68be05dbc9c95e0da92
ofentse loeto <ohentseloeto610@gmail.com>
2023-11-15 13:20:45 +0000

    Create the user object

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -64,80 +64,9 @@ class DB:
         # Return the created user object
         return user
 
-# Auth module
-
-from typing import Optional
-
-class Auth:
-    """Auth class
-    """
-
-    def __init__(self, db: DB) -> None:
-        """Initialize a new Auth instance
-
-        Args:
-            db (DB): The database instance
+    def find_user_by(self, **kwargs):
         """
-        self._db = db
-
-    def register_user(self, email: str, hashed_password: str) -> Optional[User]:
-        """Register a new user
-
-        Args:
-            email (str): User's email
-            hashed_password (str): User's hashed password
-
-        Returns:
-            Optional[User]: The created User object or None if registration fails
-        """
-        existing_user = self.get_user_by_email(email)
-        if existing_user:
-            return None  # User with this email already exists
-
-        return self._db.add_user(email, hashed_password)
-
-    def get_user_by_email(self, email: str) -> Optional[User]:
-        """Get a user by email
-
-        Args:
-            email (str): User's email
-
-        Returns:
-            Optional[User]: The User object or None if not found
-        """
-        session = self._db._session  # Only public methods of DB should be used
-        return session.query(User).filter_by(email=email).first()
-
-
-# Flask app
-
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-db = DB()
-auth = Auth(db)
-
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    email = data.get('email')
-    hashed_password = data.get('hashed_password')
-
-    if not email or not hashed_password:
-        return jsonify({'error': 'Missing required fields'}), 400
-
-    user = auth.register_user(email, hashed_password)
-    if user:
-        return jsonify({'message': 'Registration successful', 'user_id': user.id})
-    else:
-        return jsonify({'error': 'User with this email already exists'}), 400
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-def find_user_by(self, **kwargs):
-        """
+        This method finds the user by id.
         """
         session = self.Session()
         try:
