58eab427e69ffdef4eb51a308604574752ed0749
root <ofentseloeto>
2023-12-27 13:43:51 +0000

    Log Formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Obfuscate specified fields in the log message
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -27,3 +28,38 @@ def filter_datum(fields: List[str], redaction: str,
         fr'\1={redaction}',
         message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting formatter class
+    """
+
+    REDACTION = "***"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the RedactingFormatter with a list of fields to obfuscate.
+
+        Arguments:
+        - fields (List[str]): A list of strings representing fields to
+                              obfuscate.
+        """
+        super(RedactingFormatter, self).__init__()
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record, obfuscating values for specified fields.
+
+        Arguments:
+        - record (logging.LogRecord): The log record to be formatted.
+
+        Returns:
+        - str: The obfuscated log message.
+        """
+        log_message = super().format(record)
+        return filter_datum(
+            self.fields, self.REDACTION, log_message, self.SEPARATOR
+        )
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -1,17 +1,12 @@
-# main.py
-
 #!/usr/bin/env python3
 """
 Main file
 """
 
-from filtered_logger import filter_datum  # Correct import
+import logging
+from filtered_logger import RedactingFormatter
 
-fields = ["password", "date_of_birth"]
-messages = [
-    "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-    "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"
-]
-
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
+message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+print(formatter.format(log_record))
