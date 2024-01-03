757702a65030f910794b92fd1748208c3943c8d3
Ofentse Loeto <ohentseloeto610@gmail.com>
2024-01-03 13:50:08 +0000

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,28 @@
 A class SessionAuth that inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Session Authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a Session ID for a user_id.
+
+        Args:
+          - user_id (str): User ID.
+
+        Returns:
+          - str: Session ID.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
